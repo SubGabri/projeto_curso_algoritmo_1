6ae8ea2b258e3bbaa9a5fbb5c1164f4d8c2fd9e7
SubGabri <sub.gabri@gmail.com>
2022-12-11 20:48:43 -0300

    Removido os comentarios do fonte.

--- operadores-relacionais.js
+++ operadores-relacionais.js
@@ -1,17 +1,5 @@
-/*
-   nota01, nota 02: real
+e
 
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   nota01 :=10
-   nota02 :=15
-   
-   se(nota01 <> nota02)entao
-     escreval("Nota01 é diferente")
-   senao
-     escreval("Nota01 é igual")
-   fimse
-   */
 
    var nota01, nota02;
 
